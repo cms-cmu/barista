5817e7f779ce94feed6e718b995104f011cd493c
Aniket Khanal <aniketk@rogue01.phys.local.cmu.edu>
2025-10-01 15:24:17 -0400

    move classifier into src

---
+++ src/classifier/ml/benchmarks/multiclass.py
@@ -0,0 +1,46 @@
+from typing import Callable, Iterable, Literal, TypedDict
+
+from src.classifier.config.state.label import MultiClass
+from torch import Tensor
+
+from src.classifier.algorithm.hist import RegularAxis
+from src.classifier.algorithm.metrics.roc import FixedThresholdROC, linear_differ
+from src.classifier.ml import BatchType
+
+
+class ROCInputType(TypedDict):
+    y_true: Tensor
+    y_score: Tensor
+    weights: Tensor | None
+
+
+class ROC(FixedThresholdROC):
+    def __init__(
+        self,
+        name: str,
+        selection: Callable[[BatchType], ROCInputType],
+        bins: RegularAxis | Iterable[float],
+        pos: Iterable[str],
+        neg: Iterable[str] = None,
+        score: Literal["differ"] | None = None,
+    ):
+        self._name = name
+        self._select = selection
+        pos = MultiClass.indices(*pos)
+        if neg is not None:
+            neg = MultiClass.indices(*neg)
+        match score:
+            case "differ":
+                score = linear_differ
+        super().__init__(
+            thresholds=bins,
+            positive_classes=pos,
+            negative_classes=neg,
+            score_interpretation=score,
+        )
+
+    def update(self, batch: BatchType):
+        super().update(**self._select(batch))
+
+    def to_json(self):
+        return {"name": self._name} | super().to_json()
