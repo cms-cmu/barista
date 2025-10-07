1e4648e5b52df1da50610b62bc8c40227129a5ec
John Alison <johnda102@gmail.com>
2025-10-07 15:29:19 -0400

    Adding PFCands

--- src/hist_tools/object/__init__.py
+++ src/hist_tools/object/__init__.py
@@ -3,5 +3,6 @@ from .lepton import Lepton
 from .muon import Muon
 from .elec import Elec
 from .vector import LorentzVector
+from .pfcand import PFCand
 
-__all__ = ['LorentzVector', 'Jet', 'Lepton', 'Muon', 'Elec']
+__all__ = ['LorentzVector', 'Jet', 'Lepton', 'Muon', 'Elec', 'PFCand']
