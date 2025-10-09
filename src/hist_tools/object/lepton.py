9daa4c5a6dbc34d549e974431c7efa5e45aa95b2
John Alison <johnalison@cmu.edu>
2025-10-09 14:01:57 -0500

    Add _PlotLeptonMeT class for extended lepton plotting
    - Introduced `_PlotLeptonMeT` class to support additional lepton-related
      histograms, including `dr`, `dphi`, and `mT`.
    - Added `nu` and `lep` attributes with specific binning configurations.
    - Updated `Lepton` class to include `plot_leptonMeT` for enhanced plotting.

--- src/hist_tools/object/lepton.py
+++ src/hist_tools/object/lepton.py
@@ -1,4 +1,5 @@
 from ...hist_tools import H
+import numpy as np
 from .vector import _PlotDiLorentzVector, _PlotLorentzVector
 
 
@@ -13,7 +14,18 @@ class _PlotLepton(_PlotCommon, _PlotLorentzVector):
 class _PlotDiLepton(_PlotCommon, _PlotDiLorentzVector):
     ...
 
+class _PlotLeptonMeT(_PlotLorentzVector):
+    ...
+    dr    = H((50, 0, 5, ('dr', R'$\Delta R$')))
+    dphi  = H((30, -np.pi, np.pi, ('dphi', R'$\Delta\phi$')))
+    mT    = H((50, 0, 300, ('mT', R'$m_{\mathrm{T}}$ [GeV]')))
+
+    nu = _PlotLorentzVector(('...', R'Nu'), 'nu',         skip=['n'], bins={"mass": (50, 0, 100)})
+    lep = _PlotLepton(('...', R'lepton'), 'lep',     skip=['n'], bins={"mass": (50, 0, 100)})
+
+
 
 class Lepton:
     plot = _PlotLepton
     plot_pair = _PlotDiLepton
+    plot_leptonMeT = _PlotLeptonMeT
