50e974e06630888153f50d52a4ad298426ca538d
cguintob <cguintob@andrew.cmu.edu>
2026-03-10 15:44:58 -0500

    3/10/2026: Added eras I3 and I2 to

--- runner.py
+++ runner.py
@@ -902,7 +902,7 @@ if __name__ == '__main__':
         '-e', '--eras',
         nargs='+',
         dest='era',
-        default=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'C01', 'C02', 'C03', 'C04', 'C11', 'C12', 'C13', 'C14', 'C3', 'C4', 'D1', 'D2', 'D01', 'D02', 'D11', 'D12', 'F1', 'F2', 'F3', 'G1', 'G2' ],
+        default=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'C01', 'C02', 'C03', 'C04', 'C11', 'C12', 'C13', 'C14', 'C3', 'C4', 'D1', 'D2', 'D01', 'D02', 'D11', 'D12', 'F1', 'F2', 'F3', 'G1', 'G2', 'I3', 'I2' ],
         help='Data era(s) to process (data only). Examples: --eras A B C.'
     )
 
