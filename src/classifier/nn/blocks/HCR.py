2d0398fe04df832cdf41b982c99d1104e59a9582
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-08-22 12:16:58 -0400

    fix(cli,nn): unpack nested analysis_config and include nn package inits

--- src/classifier/nn/blocks/HCR.py
+++ src/classifier/nn/blocks/HCR.py
@@ -7,9 +7,4 @@ warnings.warn(
     stacklevel=2,
 )
 
-try:
-    from coffea4bees.classifier.nn.blocks.HCR import *  # noqa: F401, F403
-    from coffea4bees.classifier.nn.blocks.HCR import __all__ as _coffea4bees_all
-    __all__ = _coffea4bees_all
-except ImportError:
-    pass
+from coffea4bees.classifier.nn.blocks.HCR import *  # noqa: F401, F403
